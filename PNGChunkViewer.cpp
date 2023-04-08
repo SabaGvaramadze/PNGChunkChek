dc865f321762a440c4a57e00da3d928e79d31999
SabaGvaramadze <79360799+SabaGvaramadze@users.noreply.github.com>
2023-04-08 14:26:28 +0000

    Add files via upload
    initial commit

---
+++ PNGChunkViewer.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+union char_int{
+	char a[4];
+	int b;
+};
+#ifdef __linux__
+void getCorrectByteOrder(char *a,const size_t size){
+	for(int i=0;i<size/2;i++){
+		char tmp;
+		tmp = a[i];
+		a[i] = a[size-i-1];
+		a[size-i-1] = tmp;
+	}
+}
+#else
+void getCorrectByteOrder(char*1,const size_t size);
+#endif
+
+string typeToHigher(char *a);
+
+int main(int argc,char **argv){
+	string fileName;
+	bool showIDAT = true;
+	if(argc < 2){
+		cout << "file path";
+		cin >>fileName;
+		cout << "show IDAT chunks? y/n";
+		char tmp;
+		do{
+			cin >> tmp;
+		}while(tmp !='y' && tmp!='n');
+		if(tmp == 'n')showIDAT = false;
+	}
+	else{
+		fileName = argv[1];
+	}
+	ifstream file(fileName,ios_base::binary);
+	unsigned char pngSignature[8] = {137,80,78,71,13,10,26,10};
+	char fileSignature[8];
+	file.read(fileSignature,8);
+	
+	for(int i=0;i<8;i++){
+		if(pngSignature[i] != (unsigned char)fileSignature[i]){
+			cout << "png signature incorrect" << endl;
+			return 1;
+		}
+	}
+	while(true){
+		char_int chunkSize;
+		file.read(chunkSize.a,4);
+		getCorrectByteOrder(chunkSize.a,4);
+		char chunkType[4];
+		file.read(chunkType,4);
+		if(typeToHigher(chunkType) != "IDAT" && (argc>2 || !showIDAT)){
+		cout << "chunk type: " << typeToHigher(chunkType) << endl;
+		cout << "chunk size: " << chunkSize.b << endl;
+		cout << "--------------------------" << endl;
+		}
+		else if(argc<=2 && showIDAT){
+		cout << "chunk type: " << typeToHigher(chunkType) << endl;
+		cout << "chunk size: " << chunkSize.b << endl;
+		cout << "--------------------------" << endl;
+		}
+		if(typeToHigher(chunkType) == "IEND"){
+			return 1;
+		}
+		char *chunkData = (char*)malloc(chunkSize.b);
+		file.read(chunkData,chunkSize.b);
+		delete(chunkData);
+		chunkData = nullptr;
+		char crc[4];
+		file.read(crc,4);
+
+	}	
+}
+
+
+
+string typeToHigher(char *a){
+	string b;
+	for(int i=0;i<4;i++){
+		if(*a+i >= 'a' && *a+i <= 'z'){
+			b+= *(a+i)+('A'-'a');
+			continue;
+		}
+		b+= *(a+i);
+	}
+	return b;
+}
+		
---
+++ README.txt
@@ -0,0 +1,6 @@
+This is a simple c++ program that reads and displays all of the png file chunk.
+
+Nothing special needed for compilation.
+
+In order to ignore IDAT chunks while using the program from the command line just give a random extra argument (it can just be gibberish)
+ 
